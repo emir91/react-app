52126dc1d8447f27f2979be0c83c39b7ed2504b8
emir91 <vranacemir@gmail.com>
2021-04-24 23:06:15 +0200

    Add content to Studnets component and style it

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,5 +1,35 @@
 const Students = () => {
-    return <h1>Students</h1>
+    return (
+        <div className="container">
+            <h1>Students</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Year of Birth</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>First Name of the Student</td>
+                        <td>Last Name of the Student</td>
+                        <td>Birth Year of the Student</td>
+                    </tr>
+                     <tr>
+                        <td>First Name of the Student</td>
+                        <td>Last Name of the Student</td>
+                        <td>Birth Year of the Student</td>
+                    </tr>
+                    <tr>
+                        <td>First Name of the Student</td>
+                        <td>Last Name of the Student</td>
+                        <td>Birth Year of the Student</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Students;
